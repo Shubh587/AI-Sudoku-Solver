8c0822bc8e8625462b4ab36b03c17a0ab14121d5
Sean <66711146+sean-balks@users.noreply.github.com>
2022-05-26 23:36:05 -0400

    Initial Commit
    app.py -> Python file with Flask app to run program on local host
    templates -> HTML templates for pages (index.html, solve.html, unsolvable.html, for user input and solutions respectively)
    static -> CSS file for styling (style.css)

---
+++ app.py
@@ -0,0 +1,45 @@
+#Dev: Sean Balakhanei
+
+from flask import Flask, render_template, redirect, request, url_for
+import algorithm
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/solve", methods=["POST", "GET"])
+def get_input():
+    # Creating matrix of spots algorithmically
+    input = []
+    spots = [[]]*9
+    y_axis = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+    for i in range(len(spots)):
+        spots[i] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+    for i in range(9):
+        for j in range(len(spots[i])):
+            spots[i][j] += y_axis[i]
+    
+    # Getting input from user and storing in input matrix
+    for i in range(len(spots)):
+        for j in range(len(spots[i])):
+            input.append(request.form[spots[i][j]])
+
+    # Creating CSP object and calling backtracking algo
+    csp = algorithm.CSP(input)
+    assignment = algorithm.backtracking_search(csp)[1]
+    isSuccess = algorithm.backtracking_search(csp)[0]
+    solution = algorithm.convert_1D_array(input, assignment, isSuccess)
+
+    # If a solution exists, display it. Otherwise, the input is invalid.
+    if solution[0] != -1:
+        return render_template("solve.html", solution=solution)
+    else:
+        return render_template("unsolvable.html", solution=input)
+
+def start_again():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
