0cecfc7143f79a719ae735c5c8cd1d19b0779bf6
Shubh Savani <sms9977@nyu.edu>
2022-05-29 16:44:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-#Dev: Sean Balakhanei
+# Dev: Sean Balakhanei
 
 from flask import Flask, render_template, redirect, request, url_for
 import algorithm
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/solve", methods=["POST", "GET"])
 def get_input():
     # Creating matrix of spots algorithmically
-    input = [[]]*9
-    spots = [[]]*9
+    input = [[]] * 9
+    spots = [[]] * 9
     y_axis = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
     for i in range(len(spots)):
         spots[i] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
@@ -22,7 +24,7 @@ def get_input():
         for j in range(len(spots[i])):
             spots[i][j] += y_axis[i]
             input[i][j] += y_axis[i]
-    
+
     # Getting input from user and storing in input matrix
     for i in range(len(spots)):
         for j in range(len(spots[i])):
@@ -31,9 +33,9 @@ def get_input():
                 input[j][i] = 0
             input[j][i] = int(input[j][i])
 
-    # Creating CSP object and calling backtracking algo
-    csp = algorithm.CSP(input)
-    if csp.is_viable():  # checks if the puzzle given can produce a viable, unique solution
+    if algorithm.is_viable(input):  # checks if the puzzle given can produce a viable, unique solution
+        # Creating CSP object and calling backtracking algo
+        csp = algorithm.CSP(input)
         csp.eliminate_domain_values()
         assignment = algorithm.backtracking_search(csp)[1]
         isSuccess = algorithm.backtracking_search(csp)[0]
@@ -47,8 +49,10 @@ def get_input():
     else:
         return render_template("unsolvable.html", solution=input)
 
+
 def start_again():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
