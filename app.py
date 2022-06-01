b1d328b4f6ae1624ff5fa2dd5346650255ad9f2a
Sean <66711146+sean-balks@users.noreply.github.com>
2022-06-01 01:01:48 -0400

    Generate Puzzle
    Added generate puzzle route.
    Will automatically input a random sudoku board for the backend to solve.
    Not complete yet. As of now, there is just one puzzle that is generated every time the script is run.

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
-# Dev: Sean Balakhanei
+#Dev: Sean Balakhanei
 
 from flask import Flask, render_template, redirect, request, url_for
 import algorithm
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
 @app.route("/solve", methods=["POST", "GET"])
 def get_input():
     # Creating matrix of spots algorithmically
-    input = [[]] * 9
-    spots = [[]] * 9
+    input = [[]]*9
+    spots = [[]]*9
     y_axis = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
     for i in range(len(spots)):
         spots[i] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
@@ -24,7 +22,7 @@ def get_input():
         for j in range(len(spots[i])):
             spots[i][j] += y_axis[i]
             input[i][j] += y_axis[i]
-
+    
     # Getting input from user and storing in input matrix
     for i in range(len(spots)):
         for j in range(len(spots[i])):
@@ -33,26 +31,29 @@ def get_input():
                 input[j][i] = 0
             input[j][i] = int(input[j][i])
 
-    if algorithm.is_viable(input):  # checks if the puzzle given can produce a viable, unique solution
-        # Creating CSP object and calling backtracking algo
-        csp = algorithm.CSP(input)
-        csp.eliminate_domain_values()
-        assignment = algorithm.backtracking_search(csp)[1]
-        isSuccess = algorithm.backtracking_search(csp)[0]
-        solution = algorithm.convert_1D_array(input, assignment, isSuccess)
+    # Creating CSP object and calling backtracking algo
+    csp = algorithm.CSP(input)
+    csp.eliminate_domain_values()
+    assignment = algorithm.backtracking_search(csp)[1]
+    isSuccess = algorithm.backtracking_search(csp)[0]
+    solution = algorithm.convert_1D_array(input, assignment, isSuccess)
 
-        # If a solution exists, display it. Otherwise, the input is invalid.
-        if isSuccess:
-            return render_template("solve.html", solution=solution)
-        else:
-            return render_template("unsolvable.html", solution=input)
+    # If a solution exists, display it. Otherwise, the input is invalid.
+    if solution[0] != -1:
+        return render_template("solve.html", solution=solution)
     else:
-        return render_template("unsolvable.html", solution=input)
+        return render_template("unsolvable.html", solution=solution)
 
-
+# Back to home page when user wants to input a new puzzle
 def start_again():
     return render_template("index.html")
 
+# Display a computer generated board to be solved
+# Not finished yet, so in the meantime it will display 1 hand chosen board for the comptuer to solve
+@app.route("/generate_board", methods=["POST"])
+def generate_board():
+    #input = [-1]*81
+    return render_template("index_generated.html", input=input)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
